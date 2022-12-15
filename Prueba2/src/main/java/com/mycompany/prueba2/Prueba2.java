1c8d0e2b21dec00252e0d661f875d2644e65b05d
PC <PC@Yaizais>
2022-12-15 12:09:16 +0100

    Primera versión de la aplicación de Yaizais

---
+++ Prueba2/src/main/java/com/mycompany/prueba2/Prueba2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba2;
+
+/**
+ *
+ * @author PC
+ */
+public class Prueba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //hoanejvervbnejrvb
+        //kbdjvger
+    }
+}
